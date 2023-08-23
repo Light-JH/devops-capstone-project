14426321cb421a5f0db484e618b1cda0f87dab36
Light-JH <phoebehu@outlook.com>
2023-08-23 15:28:43 -0400

    added security hearders

--- service/__init__.py
+++ service/__init__.py
@@ -9,11 +9,13 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 from flask_talisman import Talisman
+from flask_cors import CORS
 
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+
 
 
 # Import the routes After the Flask app is created
@@ -38,4 +40,6 @@ except Exception as error:  # pylint: disable=broad-except
     sys.exit(4)
 
 app.logger.info("Service initialized!")
+
 talisman = Talisman(app)
+CORS(app)
